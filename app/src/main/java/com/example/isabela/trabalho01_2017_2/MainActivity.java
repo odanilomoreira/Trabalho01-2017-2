fd025e6e8621df2a6f4190f4c3403eea8d72455c
odanilomoreira <danilokof7@gmail.com>
2017-11-13 21:53:08 -0200

    codigo e layout principal

--- app/src/main/java/com/example/isabela/trabalho01_2017_2/MainActivity.java
+++ app/src/main/java/com/example/isabela/trabalho01_2017_2/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.isabela.trabalho01_2017_2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnCalcular;
+    private EditText txtNumero;
+    private EditText txtSalario;
+    private EditText txtNome;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        txtNome = (EditText) findViewById(R.id.txt_nome);
+        txtSalario = (EditText) findViewById(R.id.txt_salario);
+        txtNumero = (EditText) findViewById(R.id.txt_numero);
+        btnCalcular = (Button) findViewById(R.id.btn_calcular);
+
+
+
     }
 }
