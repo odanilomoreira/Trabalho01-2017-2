68ac138173692f9ccfb9c71270f2d6863e378ca3
odanilomoreira <danilokof7@gmail.com>
2017-11-13 22:21:43 -0200

    Codigo completo

--- app/src/main/java/com/example/isabela/trabalho01_2017_2/MainActivity.java
+++ app/src/main/java/com/example/isabela/trabalho01_2017_2/MainActivity.java
@@ -1,12 +1,13 @@
 package com.example.isabela.trabalho01_2017_2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     private Button btnCalcular;
     private EditText txtNumero;
@@ -25,7 +26,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         txtNumero = (EditText) findViewById(R.id.txt_numero);
         btnCalcular = (Button) findViewById(R.id.btn_calcular);
 
-
+        btnCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SegundaActivity.class);
+                intent.putExtra("txtNome",txtNome.getText().toString());
+                startActivity(intent);
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/isabela/trabalho01_2017_2/SegundaActivity.java
+++ app/src/main/java/com/example/isabela/trabalho01_2017_2/SegundaActivity.java
@@ -1,7 +1,11 @@
 package com.example.isabela.trabalho01_2017_2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class SegundaActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class SegundaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda);
+
+        Intent intent = getIntent();
+        String Nome = (String) intent.getSerializableExtra("Nome");
+
+        TextView txtNome = (TextView) findViewById(R.id.txt_nome);
+        txtNome.setText("Ola "+Nome+" seja bem vindo");
+
+
     }
 }
